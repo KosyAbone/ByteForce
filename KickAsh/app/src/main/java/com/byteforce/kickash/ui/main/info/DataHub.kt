3dfeffdf8f3d76d0feaef7676f638eced503e547
Subash <200492356@student.georgianc.on.ca>
2023-06-25 12:47:17 -0400

    Fixes

---
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/info/DataHub.kt
@@ -0,0 +1,87 @@
+package com.byteforce.kickash.ui.main.info
+
+object DataHub {
+
+    val articles = listOf(
+        ArticleModel(
+            1,
+            "The Health Risks of Smoking and the Path to Quitting",
+            "",
+            "2023-06-15",
+            "John Doe",
+            "https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
+        ),
+        ArticleModel(
+            2,
+            "Discover the Benefits of Being Smoke-Free",
+            "",
+            "2023-06-20",
+            "Jane Smith",
+            "https://images.unsplash.com/photo-1660412377714-febabe778ea7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c21va2UlMjBmcmVlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+        ),
+        ArticleModel(
+            3,
+            "How to Overcome Nicotine Addiction and Quit Smoking",
+            "",
+            "2023-06-23",
+            "Alex Johnson",
+            "https://plus.unsplash.com/premium_photo-1661674304952-9b00876d9e91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        ),
+        ArticleModel(
+            4,
+            "Understanding the Impact of Smoking on Your Lungs",
+            "",
+            "2023-06-25",
+            "Sarah Williams",
+            "https://images.unsplash.com/photo-1610975727119-cfdc910e2333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cXVpdCUyMHNtb2tpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+        ),
+        ArticleModel(
+            5,
+            "The Psychological Effects of Quitting Smoking",
+            "",
+            "2023-06-27",
+            "Michael Brown",
+            "https://images.unsplash.com/photo-1502320282078-50af0ebdb6cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cXVpdCUyMHNtb2tpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+        ),
+        ArticleModel(
+            6,
+            "10 Strategies to Help You Quit Smoking for Good",
+            "",
+            "2023-06-30",
+            "Emily Davis",
+            "https://images.unsplash.com/photo-1620325867502-221cfb5faa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1157&q=80"
+        ),
+        ArticleModel(
+            7,
+            "The Role of Support Systems in Smoking Cessation",
+            "",
+            "2023-07-02",
+            "David Wilson",
+            "https://images.unsplash.com/photo-1579208570378-8c970854bc23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=922&q=80"
+        ),
+        ArticleModel(
+            8,
+            "From Smoker to Non-Smoker: Celebrating Success Stories",
+            "",
+            "2023-07-05",
+            "Jennifer Adams",
+            "https://images.unsplash.com/photo-1541976844346-f18aeac57b06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+        ),
+        ArticleModel(
+            9,
+            "The Financial Benefits of Quitting Smoking",
+            "",
+            "2023-07-08",
+            "Robert Thompson",
+            "https://images.unsplash.com/photo-1579621970795-87facc2f976d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        ),
+        ArticleModel(
+            10,
+            "Exploring Alternative Methods to Quit Smoking",
+            "",
+            "2023-07-10",
+            "Jessica Turner",
+            "https://images.unsplash.com/photo-1543286386-2e659306cd6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        )
+    )
+}
