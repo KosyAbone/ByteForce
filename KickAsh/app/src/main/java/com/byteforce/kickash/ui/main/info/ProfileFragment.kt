1a7d28f1701338ccb8c5dc03c6a13a391b6f669a
Suraj <surajsubedi5678@gmail.com>
2023-07-15 19:00:39 +0545

    Added profile edit section

--- KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/info/ProfileFragment.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/info/ProfileFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.byteforce.kickash.databinding.FragmentNotificationsBinding
+import com.byteforce.kickash.databinding.FragmentProfileBinding
 
-class InfoFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +26,7 @@ class InfoFragment : Fragment() {
         val infoViewModel =
             ViewModelProvider(this).get(InfoViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
@@ -41,11 +39,11 @@ class InfoFragment : Fragment() {
     fun initUI() {
 
 
-        binding.rvArticles.adapter = ArticleAdapter(DataHub.articles) {
-
-        }
-
-        binding.rvArticles.layoutManager = LinearLayoutManager(requireContext())
+//        binding.rvArticles.adapter = ArticleAdapter(DataHub.articles) {
+//
+//        }
+//
+//        binding.rvArticles.layoutManager = LinearLayoutManager(requireContext())
 
 
     }
