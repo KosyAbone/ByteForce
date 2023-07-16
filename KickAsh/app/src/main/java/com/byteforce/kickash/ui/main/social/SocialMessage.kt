3972b221fff4c7e1b0088a39caee1953234d4736
builder-jc4949 <200558522@student.georgianc.on.ca>
2023-07-16 10:43:01 -0400

    Basic structure for social screen

---
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/social/SocialMessage.kt
@@ -0,0 +1,22 @@
+package com.byteforce.kickash.ui.main.social
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+data class SocialMessage(
+    var _id: String,
+    var index: Int, //Not sure if needed
+    var senderId: String, //Probably the document id for the user, not sure what the format is yet
+    var timestamp: Long,
+    var messageBody: String
+) {
+    fun getTimeString(): String {
+        val date = Date(this.timestamp)
+
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+
+        return format.format(date)
+    }
+}
