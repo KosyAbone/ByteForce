c1931e0edbfc343ebf60c9857b2b579976979053
Gautamsai Guntupalli <200557977@student.georgianc.on.ca>
2023-08-06 00:35:02 -0400

    History date popup update changes

--- KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/history/HistoryFragment.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/history/HistoryFragment.kt
@@ -1,17 +1,31 @@
 package com.byteforce.kickash.ui.main.history
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.app.DatePickerDialog
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.byteforce.kickash.R
 import com.byteforce.kickash.databinding.FragmentHistoryBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class HistoryFragment : Fragment(R.layout.fragment_history) {
+
+
+    private lateinit var frombutton: Button
+    private lateinit var tobutton: Button
     private var _binding: FragmentHistoryBinding? = null
     private val binding get() = _binding!!
+
+   // constructor()
 
 
     override fun onCreateView(
@@ -20,12 +34,38 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        frombutton = binding.FromDateTimePicker;
+        tobutton = binding.ToDateTimePicker;
+        val myCalendar = Calendar.getInstance();
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR,year)
+            myCalendar.set(Calendar.MONTH,month)
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLable(myCalendar)
+        }
+        val datePicker1 = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR,year)
+            myCalendar.set(Calendar.MONTH,month)
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLable1(myCalendar)
+        }
+
+        frombutton.setOnClickListener {
+            context?.let { it1 -> DatePickerDialog(it1,datePicker,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show() }
+        }
+
+        tobutton.setOnClickListener {
+            context?.let { it1 -> DatePickerDialog(it1,datePicker1,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show() }
+        }
         val recyclerView = binding.HistoryRV;
 
         recyclerView.layoutManager = LinearLayoutManager(context);
@@ -52,5 +92,18 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
         recyclerView.adapter = adapter
 
     }
+
+    private fun updateLable1(myCalendar: Calendar) {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.CANADA)
+        tobutton.setText(sdf.format(myCalendar.time))
+    }
+
+    private fun updateLable(myCalendar: Calendar) {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.CANADA)
+        frombutton.setText(sdf.format(myCalendar.time))
+    }
+
 
 }
