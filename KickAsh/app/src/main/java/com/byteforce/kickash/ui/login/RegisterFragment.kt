fa9f7d214d95171dc6589b02d6a14d314ab00bec
Subash <200492356@student.georgianc.on.ca>
2023-08-13 16:51:19 -0400

    Fixes
    Update data to firebase aswell

--- KickAsh/app/src/main/java/com/byteforce/kickash/ui/login/RegisterFragment.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/login/RegisterFragment.kt
@@ -9,8 +9,10 @@ import com.byteforce.kickash.R
 import com.byteforce.kickash.data.api.KickAshApi
 import com.byteforce.kickash.data.api.RegistrationData
 import com.byteforce.kickash.data.api.RegistrationResponse
+import com.byteforce.kickash.data.db.QuestionairData
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.FirebaseFirestore
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -70,11 +72,12 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             ) {
                 if (response.isSuccessful) {
                     val registrationResponse = response.body()
+
+                    saveUserData(registrationData)
                     registrationResponse?.let {
                         Log.d("RegisterActivity", "Success: ${it.message}")
                         Toast.makeText(activity, "Register successful", Toast.LENGTH_SHORT).show()
 
-                        requireActivity().onBackPressed()
 
                     } ?: run {
                         showToast("Invalid response format.")
@@ -87,18 +90,66 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
                         Toast.LENGTH_SHORT
                     ).show()
 
-                    showToast( "Error: $errorBody")
+                    showToast("Error: $errorBody")
                 }
             }
 
             override fun onFailure(call: Call<RegistrationResponse>, t: Throwable) {
-                showToast( "Network error: ${t.message}")
+                showToast("Network error: ${t.message}")
             }
         })
     }
 
-    fun showToast(msg:String) {
-        Toast.makeText(requireActivity(),msg,Toast.LENGTH_SHORT).show()
+
+
+    fun saveUserData(registrationData: RegistrationData) {
+
+
+        FirebaseFirestore.getInstance().collection(registrationData.username)
+            .add(FbData(
+                registrationData.firstName,
+                registrationData.lastName,
+                registrationData.email,
+                registrationData.username,
+                ""
+            ))
+            .addOnCompleteListener {
+                requireActivity().onBackPressed()
+
+            }
+
     }
 
+
+    fun showToast(msg: String) {
+        Toast.makeText(requireActivity(), msg, Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+
+data class FbData(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val username: String,
+    val gender: String,
+    val questionair: Questionair = Questionair(),
+    val history: List<SmokingHistory> = listOf()
+) {
+    data class Questionair(
+        val startSmokingDate: String = "",
+        val noOfCigarettePerDay: Int = 0,
+        val feelSmoking: String = "",
+        val triggerSmoking: String = "",
+        val stressfulMeter: Int = 0,
+        val promptDecision: String = "",
+        val hobbies: List<String> = listOf(),
+    )
+
+    data class SmokingHistory(
+        val id: Int = 0,
+        val dateTime: Long = 0,
+        val isRelapsed: Boolean = false
+    )
 }
