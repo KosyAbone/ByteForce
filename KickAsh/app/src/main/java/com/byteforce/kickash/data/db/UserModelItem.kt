eef1874bc66ac1cd471c32a6155211b782e94900
Subash <200492356@student.georgianc.on.ca>
2023-08-12 22:55:27 -0400

    Firebase firestore / ktx lib

--- KickAsh/app/src/main/java/com/byteforce/kickash/data/db/UserModelItem.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/data/db/UserModelItem.kt
@@ -13,3 +13,41 @@ data class UserModelItem(
     var userName:String = "",
     var password:String = ""
 )
+
+/**
+ * UserID -> {
+ *       firstName:String,
+ *       lastName: String,
+ *       email :String,
+ *       username:String,
+ *       gender :String
+ *       Questionair -> {
+ *              start_smoking: String
+ *              no_of_cigartee_per_day :String,
+ *              feel_smoking: String,
+ *              trigger_smoking:String,
+ *              stressful_meter:Int,
+ *              prompt_decision:String,
+ *              hobbies:[String],
+ *       Smoking_History -> [ {
+ *                id: Int,
+ *                date_time: Long,
+ *                is_relapsed: Boolean,
+ *       }
+ *
+ * Articles -> [
+ *          {
+ *          id: Int,
+ *          title: String,
+ *          content:String,
+ *          thumbnail_url:String,
+ *          posted_by:String,
+ *          posted_at: String,
+ *          by_admin:Boolean
+
+ * ]
+ *
+ *       ]
+ *
+ * }
+ */
