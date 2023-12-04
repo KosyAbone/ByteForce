0e3edc9bceffde58f9cec63d0e1d433fc54c6681
Subash_Aryal <200492356@student.georgianc.on.ca>
2023-12-04 14:20:18 -0500

    Fixed video plaback issue

--- KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/articles/ArticlesDetailActivity.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/ui/main/articles/ArticlesDetailActivity.kt
@@ -1,16 +1,20 @@
 package com.byteforce.kickash.ui.main.articles
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.byteforce.kickash.R
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.material.appbar.MaterialToolbar
@@ -54,29 +58,48 @@ class ArticlesDetailActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.article_author).text =
             "By: Admin · ${data.postedBy}· ${data.datePosted}"
 
-        loadPlayer(data.imageUrl)
 
     }
 
     var player: ExoPlayer? = null
 
-    var playerView: PlayerView? = null
+    var playerView: StyledPlayerView? = null
 
     fun loadPlayer(url: String) {
 
-        player = ExoPlayer.Builder(this).build()
 
-        playerView?.player = player
+        Toast.makeText(this,"Loading video",Toast.LENGTH_LONG).show()
 
-        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
-        val mediaSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(MediaItem.fromUri(""))
+//        player = SimpleExoPlayer.Builder(this).build()
+////        player = ExoPlayer.Builder(this).build()
+//
+        playerView = findViewById(R.id.playerView)
+//
+//        playerView?.player = player
+//
+//        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+//        val mediaSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+//            .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
+//
+//
+//        // Prepare the player with the media source
+//        player?.setMediaSource(mediaSource)
+//        player?.prepare()
+//
+//        player?.playWhenReady = true // Auto play
 
-
-        // Prepare the player with the media source
-        player?.setMediaSource(mediaSource)
-        player?.prepare()
-        player?.playWhenReady = true // Auto play
+        player = ExoPlayer.Builder(this@ArticlesDetailActivity).build()
+        playerView!!.player = player
+        val mediaItem: MediaItem = MediaItem.fromUri("https://video.blender.org/download/videos/3d95fb3d-c866-42c8-9db1-fe82f48ccb95-804.mp4")
+        player!!.setMediaItem(mediaItem)
+        player!!.prepare()
+        player!!.playWhenReady = true
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        player?.setPlayWhenReady(false);
+        player?.release();
+        player = null;    }
 }
