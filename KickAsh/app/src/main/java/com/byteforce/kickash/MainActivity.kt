9b0fa7be87ba8b1918e50a4a4f81ac72ffc2794f
Subash <200492356@student.georgianc.on.ca>
2023-12-04 20:42:07 -0500

    Fixes

--- KickAsh/app/src/main/java/com/byteforce/kickash/MainActivity.kt
+++ KickAsh/app/src/main/java/com/byteforce/kickash/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.byteforce.kickash
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -13,6 +23,7 @@ import com.byteforce.kickash.databinding.ActivityMainBinding
 import com.byteforce.kickash.ui.base.BaseActivity
 import com.byteforce.kickash.utils.NotificationReceiver
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.Calendar
 
 
@@ -33,13 +44,29 @@ class MainActivity : BaseActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_reward, R.id.navigation_social, R.id.navigation_info
+                R.id.navigation_home,
+                R.id.navigation_reward,
+                R.id.navigation_social,
+                R.id.navigation_info
             )
         )
         //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        scheduleNotification();
+
+
+
+        if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            scheduleNotification()
+        } else {
+
+            if (Build.VERSION.SDK_INT >= 33) {
+                notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                hasNotificationPermissionGranted = true
+            }
+
+        }
     }
 
     private fun scheduleNotification() {
@@ -52,11 +79,13 @@ class MainActivity : BaseActivity() {
 //        )
 
 
-      var  pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        var pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE)
         } else {
-            PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getActivity(
+                this, 0, notificationIntent,
+                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+            )
         }
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -65,7 +94,8 @@ class MainActivity : BaseActivity() {
         val calendar: Calendar = Calendar.getInstance()
 
         calendar.setTimeInMillis(System.currentTimeMillis())
-        calendar.set(Calendar.HOUR_OF_DAY, 12) // Set the hour
+        calendar.add(Calendar.DAY_OF_YEAR, 1); // tomoorw
+        calendar.set(Calendar.HOUR_OF_DAY, 2) // Set the hour
         calendar.set(Calendar.MINUTE, 0) // Set the minute
 
         // Repeat the alarm every day
@@ -75,5 +105,104 @@ class MainActivity : BaseActivity() {
             AlarmManager.INTERVAL_DAY,
             pendingIntent
         )
+    }
+
+
+
+
+    private val pushNotificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            scheduleNotification()
+        }
+
+
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            hasNotificationPermissionGranted = isGranted
+            if (!isGranted) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Build.VERSION.SDK_INT >= 33) {
+                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                            showNotificationPermissionRationale()
+                        } else {
+                            showSettingDialog()
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "notification permission granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+
+    private fun showSettingDialog() {
+        MaterialAlertDialogBuilder(
+            this,
+            com.google.android.material.R.style.MaterialAlertDialog_Material3
+        )
+            .setTitle("Notification Permission")
+            .setMessage("Notification permission is required, Please allow notification permission from setting")
+            .setPositiveButton("Ok") { _, _ ->
+                val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.parse("package:$packageName")
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun showNotificationPermissionRationale() {
+
+        MaterialAlertDialogBuilder(
+            this,
+            com.google.android.material.R.style.MaterialAlertDialog_Material3
+        )
+            .setTitle("Info")
+            .setMessage("Notification permission is required, to show notification")
+            .setPositiveButton("Ok") { _, _ ->
+                if (Build.VERSION.SDK_INT >= 33) {
+                    notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    var hasNotificationPermissionGranted = false
+
+
+    private fun showNotification() {
+
+        val channelId = "12345"
+        val description = "Test Notification"
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.lightColor = Color.BLUE
+
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+
+        val  builder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Test TItle")
+            .setContentText("Show Ton")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    this.resources, R.drawable
+                        .ic_launcher_background
+                )
+            )
+        notificationManager.notify(12345, builder.build())
     }
 }
